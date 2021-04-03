3996ceaa2f1485411a0726dd66ccae9e28d23907
Nick Meinhold <nick.meinhold@gmail.com>
2021-04-03 20:09:05 +1100

    Add improvements to ApiTags class

--- lib/api_tags.dart
+++ lib/api_tags.dart
@@ -3,23 +3,31 @@ import 'package:html/parser.dart';
 import 'package:http/http.dart';
 
 class ApiTags {
+  static const httpsScheme = 'https';
+  static const meetupHost = 'www.meetup.com';
+  static const docsPath = '/meetup_api/docs/';
+
+  final client = Client();
+
   /// The API documentation opens with a list where each item is an index into a
   /// detail view
   late final Element indexTags;
 
-  static Future<Element?> retrieveApiIndexTags() async {
-    final client = Client();
-    final response = await client.get(Uri(
-        scheme: 'https', host: 'www.meetup.com', path: '/meetup_api/docs/'));
+  Future<Element> retrieveApiIndexTags() async {
+    final response = await client
+        .get(Uri(scheme: httpsScheme, host: meetupHost, path: docsPath));
 
     // Use html parser and query selector
     final document = parse(response.body);
     final indexTags = document.getElementById('api-index');
 
-    return indexTags;
+    return indexTags ?? Element.tag('empty');
   }
 
-  static Future<Element?> retrieveApiDetailTags() async {
-    // return document.getElementById('method-info');
+  Future<Element> retrieveApiDetailTags(String endpoint) async {
+    final response = await client.get(
+        Uri(scheme: httpsScheme, host: meetupHost, path: docsPath + endpoint));
+    final document = parse(response.body);
+    return document.getElementById('method-info') ?? Element.tag('empty');
   }
 }
